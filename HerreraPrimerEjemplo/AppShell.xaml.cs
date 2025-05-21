f40da8e6d64d333617e9b1043d72b5084c746c49
laboratorio <laboratorio@UPOAULA10710>
2025-05-21 08:35:24 -0500

    Agregar archivos de proyecto.

---
+++ HerreraPrimerEjemplo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HerreraPrimerEjemplo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HerreraPrimerEjemplo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HerreraPrimerEjemplo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
